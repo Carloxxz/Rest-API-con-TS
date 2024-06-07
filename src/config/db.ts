80fe434f28d400982206b401c1364bcb0174d772
Carlos Martínez <carloxmz@gmail.com>
2024-06-07 16:31:01 -0600

    - Servicio de db No local agregado

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,12 +2,20 @@ import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv"
 dotenv.config()
 
-export const db = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+export const db = new Sequelize(process.env.DATABASE_URL, {
     host: 'localhost',
     dialect: 'postgres',
     models: [__dirname + '/../models/**/*'],
     logging: false
 })
 
+
+// Conexión local
+// export const db = new Sequelize(process.env.DATABASE, process.env.USERNAME, process.env.PASSWORD, {
+//     host: 'localhost',
+//     dialect: 'postgres',
+//     models: [__dirname + '/../models/**/*'],
+//     logging: false
+// })
 
 export default db
